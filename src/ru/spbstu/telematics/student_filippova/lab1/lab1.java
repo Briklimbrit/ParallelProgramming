6cce32829b23573bd9dfdf6b9d23832e24f4e2e3
Elena <helenfilova@gmail.com>
2018-10-12 11:00:09 +0300

    Bugfix

---
+++ src/ru/spbstu/telematics/student_filippova/lab1/lab1.java
@@ -0,0 +1,14 @@
+package ru.spbstu.telematics.student_filippova.lab1;
+
+public class lab1 {
+
+	public lab1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
